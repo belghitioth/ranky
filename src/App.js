9585c6f2624d05f28521f7219d739f53e3587a76
othmaneeb <belghitioth@gmail.com>
2019-07-07 20:41:14 +0200

    Add cards with bars using Firebase

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import { createStyles, makeStyles } from '@material-ui/core/styles';
+import React, {Component} from 'react';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
@@ -7,67 +6,92 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import * as firebase from 'firebase';
 import './App.css';
 import dj from './img/dj.jpg'
 
+var firebaseConfig = {
+    apiKey: "AIzaSyAPThA_o-fCWAFWbTKmxAgxLwIOUrV3XDg",
+    authDomain: "ranky-b627b.firebaseapp.com",
+    databaseURL: "https://ranky-b627b.firebaseio.com",
+    projectId: "ranky-b627b",
+    storageBucket: "ranky-b627b.appspot.com",
+    messagingSenderId: "102594017401",
+    appId: "1:102594017401:web:a1c2eff7534c90b9"
+};
 
-const useStyles = makeStyles(
-  createStyles({
-    card: {
-      maxWidth: 345,
-    },
-    media: {
-      height: 140,
-    },
-  }),
-);
+firebase.initializeApp(firebaseConfig)
 
-function App() {
-    const classes = useStyles();
-    const bars = [
-        { id: 1, name: 'Badaboom' , description: ' The best bar of Paris !'},
-        { id: 2, name: 'Le 43 Cocktail Bar' , description: ' The best bar of Paris !'},
-        { id: 3, name: 'The Financier', description: ' The best bar of Paris !' },
-        { id: 4, name: 'Wanderlust', description: ' The best bar of Paris !' },
-        { id: 5, name: 'Le Perchoir', description: ' The best bar of Paris !' },
-    ]
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Ranky - Book a Venue</h1>
-      </header>
-      <div className="Cards"  >
-          {bars.map((bar) => (
-          <Card className="card">
-          <CardActionArea>
-            <CardMedia
-              className="media"
-              image={dj}
-              title={bar.name}
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="h2">
-                {bar.name}
-              </Typography>
-              <Typography variant="body2" color="textSecondary" component="p">
-                {bar.description}
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-          <CardActions>
-            <Button size="small" color="primary">
-              Book
-            </Button>
-            <Button size="small" color="primary">
-              Learn More
-            </Button>
-          </CardActions>
-        </Card>
-          ))}
-      </div>
-    </div>
-  );
+class App extends Component{
+
+    constructor(  ) {
+        super();
+        this.state= {
+            number_of_bars: 10,
+            bars: [],
+        };
+    }
+
+    componentDidMount() {
+        let rootRef = firebase.database().ref();
+        let barsRef= rootRef.child('bars');
+
+        barsRef.on('value',snap =>{
+            let bars =snap.val();
+            let newState = [];
+            for (let bar in bars) {
+                newState.push({
+                    name: bars[bar].name,
+                    description: bars[bar].description,
+                });
+            }
+            this.setState({
+                bars: newState
+            });
+        })
+    };
+
+
+    render() {
+
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1>Buzz-link - Book a Venue</h1>
+                </header>
+                <div className="Cards">
+                    {this.state.bars.map(( bar ) => (
+                        <Card className="card">
+                            <CardActionArea>
+                                <CardMedia
+                                    className="media"
+                                    image={dj}
+                                    title={bar.name}
+                                />
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="h2">
+                                        {bar.name}
+                                    </Typography>
+                                    <Typography variant="body2" color="textSecondary" component="p">
+                                        {bar.description}
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                            <CardActions>
+                                <Button size="small" color="primary">
+                                    Book
+                                </Button>
+                                <Button size="small" color="primary">
+                                    Learn More
+                                </Button>
+                            </CardActions>
+                        </Card>
+                    ))}
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
